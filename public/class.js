eda2f095f6489081cda6af2ca84b60ede1dfa525
Camille Marcq <camillemarcq@MacBook-Pro-de-Camille.local>
2021-02-08 10:01:33 +0100

    start

---
+++ public/class.js
@@ -0,0 +1,13 @@
+// créer une classe personne
+// créer un nouvel objet = une instance
+
+//créer class lieu (nom, personne [])
+// maison, épicerie
+
+// créer outil (pas de classe needed)
+// couteau nom + action (méthode pour changer l'état)
+
+// créer ingrédients (en stock dans l'épicerie) avec "nom" "état"(coupé moulu entier) et un prix
+// 6 ingrédients à stocker dans un tableau + ajouter un panier
+
+//fonction set time out ? pour faire cuire après quelques secondes.
